7dae8bf1a3656ea3b8fc9858dc60476e68903c04
Fahrradschieber <mail@drbert.de>
2018-01-03 23:33:35 +0100

    Powershell Update

--- FactZip.ps1
+++ FactZip.ps1
@@ -1,14 +1,17 @@
 # ---------------------------------------------------------------
-# Script: Factorio Modzipping for cheesecake
-# Author: MadUrban
+# Script: Factorio Modzipping for advanced-atomics
+# Author: MadUrban,drbert
 # comments: .Net Framework 4.5 required
 #----------------------------------------------------------------
 $mydocuments = [Environment]::GetFolderPath('MyDocuments')
-$source = $mydocuments + "\github\cheesecake\data"
-$destination = $mydocuments + "\github\cheesecake\bin\Cheesecake_0.1.0.zip"
-$modfolder = $env:APPDATA + "\factorio\mods\"
 
-If(Test-path $destination) {Remove-item $destination}
+$source = "$mydocuments\github\advanced-atomics\data"
+$info = Get-Content "$source\info.json" | ConvertFrom-Json
+
+$destination = "$mydocuments\github\advanced-atomics\bin\advanced-atomics_$($info.version).zip"
+$modfolder = "$env:APPDATA\factorio\mods\"
+
+#If(Test-path $destination) {Remove-item $destination}
 Add-Type -assembly "system.io.compression.filesystem"
 [system.io.compression.zipfile]::CreateFromDirectory($source, $destination)
 
