25b37cea18813933c2a25aa706642526d17903dc
Fahrradschieber <mail@drbert.de>
2018-01-04 00:25:20 +0100

    new bugs to fix
    implemented equipment and plutonium armor

--- data/prototypes/UraniumArmor.lua
+++ data/prototypes/UraniumArmor.lua
@@ -11,7 +11,7 @@ data:extend{
 data:extend{
 	{
 		type = "armor",
-		name = "klaproth-armor",
+		name = "uranium-armor",
 		--icon = "PATH",
 		icon_size = 32,
 		flags = {"goes-to-main-inventory"}
@@ -40,9 +40,65 @@ data:extend{
     },
 	durability = 50000,
 	subgroup = "armor",
-	order = "h(klaproth-armor)",
+	order = "h(uranium-armor)",
 	stack-size = 1,
 	equipment_grid = "larger-equipment-grid",
 	inventory_size_bonus = 70
-	}
+  },
+  
+
+  --- Plutonium Armor
+  {
+    type = "armor",
+    name = "plutonium-armor",
+    --icon = "PATH",
+    flags = {"goes-to-main-inventory"},
+    resistances =
+    {
+      {
+        type = "physical",
+        decrease = 30,
+        percent = 60
+      },
+      {
+        type = "acid",
+        decrease = 30,
+        percent = 60
+      },
+      {
+        type = "explosion",
+        decrease = 40,
+        percent = 60
+      },
+      {
+        type = "fire",
+        decrease = 0,
+        percent = 100
+      }
+    },
+    durability = 40000,
+    subgroup = "armor",
+    order = "g[plutonium-armor]",
+    stack_size = 1,
+    equipment_grid = "larger-equipment-grid",
+    inventory_size_bonus = 60
+	
+  },
+
+  {
+    type = "recipe",
+    name = "plutonium-armor",
+    enabled = false,
+    energy_required = 120,
+    ingredients = {
+	  {"uranium-armor", 1},
+	  {"Pu-239", 42},
+	  {"fusion-reactor-equipment", 2},
+	  {"processing-unit", 300},
+	  {"low-density-structure", 200}
+	},
+    result = "plutonium-armor",
+    requester_paste_multiplier = 1
+  }
+
 }
---
+++ data/prototypes/equipment.lua
@@ -0,0 +1,130 @@
+data:extend(
+{  
+
+  --- Uranium Shield
+  {
+  type = "item",
+  name = "uranium-shield-equipment",
+  icon = "__Advanced-Atomics__/graphics/icons/uranium-shield-equipment.png",
+  placed_as_equipment_result = "uranium-shield-equipment",
+  flags = {"goes-to-main-inventory"},
+  subgroup = "equipment",
+  order = "b[shield]-c[uranium-shield-equipment]",
+  stack_size = 50,
+  default_request_amount = 10
+ },
+   {
+    type = "energy-shield-equipment",
+    name = "uranium-shield-equipment",
+    sprite =
+    {
+      filename = "__Advanced-Atomics__/graphics/equipment/uranium-shield-equipment.png",
+      width = 64,
+      height = 64,
+      priority = "medium"
+    },
+    shape =
+    {
+      width = 3,
+      height = 3,
+      type = "full"
+    },
+    max_shield_value = 400,
+    energy_source =
+    {
+      type = "electric",
+      buffer_capacity = "480kJ",
+      input_flow_limit = "500kW",
+      usage_priority = "primary-input"
+    },
+    energy_per_shield = "80kJ",
+    categories = {"armor"}
+  },
+
+  --- Plutonium Shield
+  {
+    type = "item",
+    name = "plutonium-shield-equipment",
+    icon = "__Advanced-Atomics__/graphics/icons/plutonium-shield-equipment.png",
+    placed_as_equipment_result = "plutonium-shield-equipment",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "equipment",
+    order = "b[shield]-c[plutonium-shield-equipment]",
+    stack_size = 50,
+    default_request_amount = 10
+   },
+     {
+      type = "energy-shield-equipment",
+      name = "plutonium-shield-equipment",
+      sprite =
+      {
+        filename = "__Advanced-Atomics__/graphics/equipment/plutonium-shield-equipment.png",
+        width = 64,
+        height = 64,
+        priority = "medium"
+      },
+      shape =
+      {
+        width = 3,
+        height = 3,
+        type = "full"
+      },
+      max_shield_value = 400,
+      energy_source =
+      {
+        type = "electric",
+        buffer_capacity = "480kJ",
+        input_flow_limit = "500kW",
+        usage_priority = "primary-input"
+      },
+      energy_per_shield = "80kJ",
+      categories = {"armor"}
+    },
+
+  --- Plutonium Battery
+  {
+    type = "item",
+    name = "radionuclide-battery",
+    icon = "__Advanced-Atomics__/graphics/icons/radionuclide-battery.png",
+    placed_as_equipment_result = "radionuclide-battery",
+    flags = {"goes-to-main-inventory"},
+    subgroup = "equipment",
+    order = "c[battery]-c[radionuclide-battery]",
+    stack_size = 50,
+    default_request_amount = 10
+  },
+
+  {
+    type = "battery-equipment",
+    name = "radionuclide-battery-equipment",
+    sprite =
+    {
+      filename = "__Advanced-Atomics__/graphics/equipment/radionuclide-battery-equipment.png",
+      width = 64,
+      height = 96,
+      priority = "medium"
+    },
+    shape =
+    {
+      width = 2,
+      height = 3,
+      type = "full"
+    },
+    energy_source =
+    {
+      type = "electric",
+      buffer_capacity = "750MJ",
+      input_flow_limit = "4GW",
+      output_flow_limit = "4GW",
+      usage_priority = "terciary"
+    },
+    categories = {"armor"}
+  }
+}
+)
+data:extend{
+  
+  
+
+  
+}  
