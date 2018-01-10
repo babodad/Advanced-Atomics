75876a8a067dca7d36e4fd33dc18528ced275289
madurban <mad42urban@gmail.com>
2018-01-10 21:35:50 +0100

    Updated PS Script
    Copies data folder to "Advanced-Atomics_$Version", archives and copies
    archive to Factorio mod folder. ZIP and folder are deleted afterwards.

--- FactZip.ps1
+++ FactZip.ps1
@@ -4,15 +4,20 @@
 # comments: .Net Framework 4.5 required
 #----------------------------------------------------------------
 $mydocuments = [Environment]::GetFolderPath('MyDocuments')
-
-$source = "$mydocuments\github\advanced-atomics\data\Advanced-Atomics"
-$info = Get-Content "$source\info.json" -Raw | ConvertFrom-Json
-
-$destination = "$mydocuments\github\advanced-atomics\bin\Advanced-Atomics_$($info.version).zip"
+$sourcedata = "$mydocuments\github\advanced-atomics\data"
 $modfolder = "$env:APPDATA\factorio\mods\"
+
+$info = Get-Content "$sourcedata\info.json" -Raw | ConvertFrom-Json
+$sourcedir = "$mydocuments\github\advanced-atomics\bin\Advanced-Atomics_$($info.version)"
+copy-item $sourcedata $sourcedir -Recurse -Force
+
+$destination = "$sourcedir.zip"
 
 If(Test-path $destination) {Remove-item $destination}
 Add-Type -assembly "system.io.compression.filesystem"
-[system.io.compression.zipfile]::CreateFromDirectory($source, $destination)
+[system.io.compression.zipfile]::CreateFromDirectory($sourcedir, $destination)
 
 Copy-Item $destination $modfolder
+
+If(Test-path $destination) {Remove-item $destination}
+If(Test-path $sourcedir) {Remove-item -path $sourcedir -Recurse}
