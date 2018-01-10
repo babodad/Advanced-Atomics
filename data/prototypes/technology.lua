aad6a250126d834135d7fa57598a57a8cc584311
Fahrradschieber <mail@drbert.de>
2018-01-10 23:03:41 +0100

    new techs for mk2-axe and U-Armor

--- data/prototypes/technology.lua
+++ data/prototypes/technology.lua
@@ -1,6 +1,7 @@
 data:extend(
 {
 
+	--- Uranium Axe (+mk2)
 	{
 		type = "technology",
 		name = "uranium-tools",
@@ -10,23 +11,60 @@ data:extend(
 			{
 				type = "unlock-recipe",
 				recipe = "uranium-axe"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "uranium-axe-mk2"
 			}
 		},
 		prerequisites = {
 			"nuclear-power"
 		},
 		unit = {
-			count = 1000,
+			count = 400
 			ingredients = {
 				{"science-pack-1", 1},
 				{"science-pack-2", 1},
-				{"science-pack-3", 1},
-				{"production-science-pack", 1},
+				{"science-pack-3", 2},
+				{"production-science-pack", 3},
 			},
 			time = 42
 		}
 	},
-
+	
+	--- Uranium Armor
+	{
+		type = "technology",
+		name = "uranium-armor",
+		icon_size = 128,
+		icon = "__Advanced-Atomics__/graphics/technology/UraniumTools_128.png",
+		effects = {
+			{
+				type = "unlock-recipe",
+				recipe = "uranium-axe"
+			},
+			{
+				type = "unlock-recipe",
+				recipe = "uranium-axe-mk2"
+			}
+		},
+		prerequisites = {
+			"nuclear-power",
+			"power-armor-2",
+			"military-4"
+		},
+		unit = {
+			count = 200,
+			ingredients = {
+				{"science-pack-1", 1},
+				{"science-pack-2", 1},
+				{"science-pack-3", 2},
+				{"military-science-pack", 3},
+				{"high-tech-science-pack", 3}
+			},
+			time = 42
+		}
+	},	
 })
 
 --table.insert(data.raw["technology"]["atomic-bomb"].effects,{type="unlock-recipe",recipe="uranium-artillery-shell"})
