02f406ea73b268b3b59bc27aa5ecefde0a4925ac
Fahrradschieber <mail@drbert.de>
2018-01-11 15:16:08 +0100

    changed plutonium color

--- data/prototypes/UraniumAxe.lua
+++ data/prototypes/UraniumAxe.lua
@@ -16,7 +16,7 @@ data:extend({
 			target_effects =
 			{
 				type = "damage",
-				damage = { amount = 10 , type = "physical"}
+				damage = { amount = 8 , type = "physical"}
 			}
 		  }
 		},
@@ -61,7 +61,7 @@ data:extend({
 			target_effects =
 			{
 				type = "damage",
-				damage = { amount = 10 , type = "physical"}
+				damage = { amount = 12 , type = "physical"}
 			}
 		  }
 		},
@@ -89,6 +89,51 @@ data:extend({
     },
     result = "uranium-axe-mk2",
     requester_paste_multiplier = 1
-  },  
+	},  
+	
+	---- Plutonium Axe
+	{
+		type = "mining-tool",
+		name = "plutonium-axe",
+		icon = "__Advanced-Atomics__/graphics/icons/plutonium-axe.png",
+		icon_size = 32,
+		flags = {"goes-to-quickbar"},
+		action =
+		{
+		  type="direct",
+		  action_delivery =
+		  {
+			type = "instant",
+			target_effects =
+			{
+				type = "damage",
+				damage = { amount = 24 , type = "physical"}
+			}
+		  }
+		},
+		durability = 101010,
+		subgroup = "tool",
+		order = "a[mining]-b[steel-axe]",---test: -d[uranium-axe-mk2]
+		speed = 18,
+		stack_size = 5,
+		build_distance_bonus = 24,
+		item_drop_distance_bonus = 24,
+		item_pickup_distance_bonus = 24,
+		loot_pickup_distance_bonus = 24,
+		resource_reach_distance_bonus = 12
+	},	
+  
+  {
+    type = "recipe",
+    name = "plutonium-axe",
+    enabled = false,
+    ingredients =
+    {
+      {"uranium-axe-mk2", 1},
+      {"plutonium-239", 42}
+    },
+    result = "plutonium-axe",
+    requester_paste_multiplier = 1
+  }
   
  })
