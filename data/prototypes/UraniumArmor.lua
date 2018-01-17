e06cd9db442a835239d1240c46fb74f7cf268652
madurban <mad42urban@gmail.com>
2018-01-17 23:45:31 +0100

    mk2 armor anim enabled
    Armor uses Power Armor Mk2 Sprite

--- data/prototypes/UraniumArmor.lua
+++ data/prototypes/UraniumArmor.lua
@@ -71,3 +71,13 @@ data:extend(
 
 })
 
+for _, animation in ipairs(data.raw["player"]["player"]["animations"]) do
+  if animation.armors then
+    for _, armor in ipairs(animation.armors) do
+      if armor == "power-armor-mk2" then
+        animation.armors[#animation.armors + 1] = "uranium-armor"
+        break
+      end
+    end
+  end
+end
