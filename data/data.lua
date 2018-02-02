c0cc3246c73b722dc86048c7558238330129bd00
babodad <mail@drbert.de>
2018-02-02 23:37:08 +0100

    New settings

--- data/data.lua
+++ data/data.lua
@@ -1,35 +1,55 @@
+---- Mod-Settings
 AA = {}
 
--- ingame config 
 AA.ENABLED = {
 	["SSP4Plutonium"] = settings.startup["AA_enable-SSP4Plutonium"].value,
-	["TreeMapColor"] = settings.startup["AA_enable-TreeMapColor"].value,	
 	["UraniumExtraction"] = settings.startup["AA_enable-UraniumExtraction"].value,
-
 }
 
--- Technologies
+-- Distance Bonus
+local distance_bonus = settings.startup["AA_DistanceBonus"].value
+
+
+if distance_bonus == "off" then
+	AA.DistFactor = 0
+elseif distance_bonus == "low" then
+	AA.DistFactor = 0.5
+elseif distance_bonus == "medium" then
+	AA.DistFactor = 1.0	
+elseif distance_bonus == "high" then
+	AA.DistFactor = 1.5
+end
+
+-- Custom Tree Map Color
+AA.TreeContrast = settings.startup["AA_TreeMapContrast"].value
+
+if AA.TreeContrast == "low" then
+	data.raw["utility-constants"]["default"]["chart"]["default_color_by_type"].tree = {a = 0.1, r = 0.1, g = 0.4, b = 0.1}
+elseif AA.TreeContrast == "medium"  then
+	data.raw["utility-constants"]["default"]["chart"]["default_color_by_type"].tree = {a = 0.4, r = 0.1, g = 0.4, b = 0.1}    
+elseif AA.TreeContrast == "high"  then
+	data.raw["utility-constants"]["default"]["chart"]["default_color_by_type"].tree = {a = 0.8, r = 1.0, g = 0.0, b = 0.0}      
+end
+-- Server command line needed to regenerate already discovered areas: Warning, chopped/removed trees will reappear!
+-- /c game.regenerate_entity("dead-dry-hairy-tree"); game.regenerate_entity("dead-grey-trunk");game.regenerate_entity("dry-hairy-tree");game.regenerate_entity("dry-tree");game.regenerate_entity("tree-01");game.regenerate_entity("tree-02");game.regenerate_entity("tree-02-red");game.regenerate_entity("tree-03");game.regenerate_entity("tree-04");game.regenerate_entity("tree-05");game.regenerate_entity("tree-06");game.regenerate_entity("tree-06-brown");game.regenerate_entity("tree-07");game.regenerate_entity("tree-08");game.regenerate_entity("tree-08-brown");game.regenerate_entity("tree-08-red");game.regenerate_entity("tree-09");game.regenerate_entity("tree-09-brown");game.regenerate_entity("tree-09-red")
+
+
+---- Technologies
 require ("prototypes.technology")
 
---- Items
-require("prototypes.uranium-axe")
+---- Items
+require("prototypes.atomic-tools")
 require("prototypes.uranium-armor")
 require("prototypes.plutonium")
 require("prototypes.plutonium-armor")
-require("prototypes.plutonium-axe")
 require("prototypes.projectiles")
 require("prototypes.ammo")
 
---- Equipment
+---- Equipment
 require("prototypes.equipment")
 
----Uranium Extraction
+---- Uranium Extraction
 if AA.ENABLED["UraniumExtraction"]  then
     require("prototypes.uranium-extraction")   
 end
---- Custom Tree Map Color
--- Following server command line needed to regenerate already discovered areas. Warning, chopped/removed trees will reappear!
--- /c game.regenerate_entity("dead-dry-hairy-tree"); game.regenerate_entity("dead-grey-trunk");game.regenerate_entity("dry-hairy-tree");game.regenerate_entity("dry-tree");game.regenerate_entity("tree-01");game.regenerate_entity("tree-02");game.regenerate_entity("tree-02-red");game.regenerate_entity("tree-03");game.regenerate_entity("tree-04");game.regenerate_entity("tree-05");game.regenerate_entity("tree-06");game.regenerate_entity("tree-06-brown");game.regenerate_entity("tree-07");game.regenerate_entity("tree-08");game.regenerate_entity("tree-08-brown");game.regenerate_entity("tree-08-red");game.regenerate_entity("tree-09");game.regenerate_entity("tree-09-brown");game.regenerate_entity("tree-09-red")
-if AA.ENABLED["TreeMapColor"]  then
-    data.raw["utility-constants"]["default"]["chart"]["default_color_by_type"].tree = {a = 0.4, r = 0.1, g = 0.4, b = 0.1}    
-end
+
--- data/settings.lua
+++ data/settings.lua
@@ -1,24 +1,34 @@
 data:extend({
 	{
         type = "bool-setting",
-		name = "AA_enable-TreeMapColor",
+		name = "AA_enable-SSP4Plutonium",
 		setting_type = "startup",
 		default_value = false,
 		order = "a-a"
 	},
 	{
         type = "bool-setting",
-		name = "AA_enable-SSP4Plutonium",
-		setting_type = "startup",
-		default_value = false,
-		order = "a-b"
-	},
-	{
-        type = "bool-setting",
 		name = "AA_enable-UraniumExtraction",
 		setting_type = "startup",
 		default_value = false,
-		order = "a-c"
-	}
+		order = "a-bc"
+	},
+	{
+		type = "string-setting",
+		name = "AA_DistanceBonus",
+		setting_type = "startup",
+		default_value = "medium",
+		allowed_values = { "off", "low", "medium", "large"}, 
+        order = "a-c"
+	},
+	{
+		type = "string-setting",
+		name = "AA_TreeMapContrast",
+		setting_type = "startup",
+		default_value = "medium",
+		allowed_values = { "off", "low", "medium", "high"}, 
+        order = "a-d"
+	},
+
 	
 })
